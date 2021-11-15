cb5f84787eb0d8ba644b573854b8339e9f05af3a
JohnW4561 <94328715+JohnW4561@users.noreply.github.com>
2021-11-14 19:15:46 -0500

    Update about.js

--- pages/about.js
+++ pages/about.js
@@ -4,28 +4,14 @@ const About = ({ title, description, ...props }) => {
   return (
     <>
       <Layout pageTitle={`${title} | About`} description={description}>
-        <h1 className="title">Welcome to this demo blog!</h1>
+        <h1 className="title">Welcome to this biking blog!</h1>
 
         <p className="description">
-          This is a simple blog built with Next, easily deployable on{' '}
-          <a href="https://url.netlify.com/r1j6ybSYU">Netlify</a>.
+         This is a little project I put together to talk about this new sport I'm getting into called trail biking.
         </p>
 
         <p>
-          You can check out the{' '}
-          <a href="https://github.com/cassidoo/next-netlify-blog-starter">
-            repo here.
-          </a>{' '}
-          If you'd like to build it yourself,{' '}
-          <a href="https://url.netlify.com/ByVW0bCF8">
-            here is a tutorial on how to do so
-          </a>
-          !
-        </p>
-
-        <p>
-          This project includes a basic layout and header, base styles, dynamic
-          routing with getStaticPaths, and posts saved as Markdown.
+          I am fairly new to trail biking but I am enjoying it immensely so far.  My plan is to build this blog while I am learning to document what I am learning and to share with others.
         </p>
       </Layout>
     </>
